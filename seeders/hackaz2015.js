abf51614270b95794ed0985380c8a39a3de1e3b5
Travis <traviswoodrow@email.arizona.edu>
2015-11-07 15:21:31 -0700

    Adds test Attendee seeder

---
+++ seeders/hackaz2015.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Attendee", [{
+      application_start: "1111-22-33 44:55:66",
+      application_submit: "1111-22-33 44:55:67",
+      name: "Travis sucks at Postgres",
+      email: "travis@shitty.postgres.com",
+      gender: "M",
+      tshirt_size: "XL",
+      diet_restriction: "",
+      school: "University of Arizona",
+      age: 26,
+      highschool_student: false,
+      require_travel_reimbursement: false,
+      first_hackathon: false,
+      github_account: "https://github.com/fuckyou",
+      linkedin_account: "https://linkedin.com/fuckyou",
+      personal_website: "https://fuck.you",
+      resume: "bits bits bits",
+      what_do_you_like_to_work_with: ["one", "two", "three"],
+    }])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("Attendee")
+  }
+};
