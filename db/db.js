427ae0ba5581478865e1cf453dd720728665f9a9
Travis <traviswoodrow@email.arizona.edu>
2015-11-07 01:28:02 -0700

    Finishes db scaffold

--- db/db.js
+++ db/db.js
@@ -1,6 +1,14 @@
-var db = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
+// imports
+var Sequelize = require("sequelize")
+
+// initialize the database
+var db = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "postgres",
 })
+
+// load models
+var Attendee = db.import("./models/attendee.js")
+
 
 module.exports = db
